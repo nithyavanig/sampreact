09978399a99a858f5064364de2c2150c57a3ce1c
nithyavanig <35562570+nithyavanig@users.noreply.github.com>
2018-05-15 15:24:44 +0530

    samp form

--- src/App.js
+++ src/App.js
@@ -1,19 +1,59 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'; 
+import RaisedButton from 'material-ui/RaisedButton';
+import TextField from 'material-ui/TextField';
 
 class App extends Component {
+  constructor(props) {
+    super(props);    
+    this.state = {
+       name: 'Hello'
+    }
+    this.handleTextChange = this.handleTextChange.bind(this);
+    this.handleSubmit=this.handleSubmit.bind(this);
+  };
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Sample React App</h1>
-        </header>
-        <p className="Form-1">
-        </p> 
+    <MuiThemeProvider>
+    <div className="App">
+      <div className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h2>Sample React App</h2>
       </div>
+      <div className="App-intro">
+          <div><label>Name</label></div>
+          <div>
+          <TextField
+              style={{ 'width': '20%' }}
+              hintText="Name"
+              floatingLabelText="Your Name"              
+              name="Name1"
+              onChange={this.handleTextChange}
+              value={this.state.name}                            
+          />
+          <br/>
+      </div>
+      <div>
+        <RaisedButton 
+                label="Submit" 
+                color="secondary" 
+                lableStyle={{float: 'left'}} 
+                onClick={this.handleSubmit} 
+        />
+      </div>
+      </div>
+    </div>
+    </MuiThemeProvider>
     );
+  }
+  handleTextChange(event) {
+    this.setState({name: event.target.value})
+  }
+  handleSubmit(event) {
+    alert('Welcome ..' + this.state.name);
+    event.preventDefault();
   }
 }
 
